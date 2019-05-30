e8056ed238622384686300b791cb49bddf2b2237
Jamie Wilkinson <jamie@jamiedubs.com>
2019-05-30 11:09:51 -0400

    More kext debugging/output caching. Thinking that kextstat output is not helpful

--- list-kexts.sh
+++ list-kexts.sh
@@ -3,3 +3,6 @@
 kextstat | grep -v com.apple > kexts-kextstat.txt
 
 ls /Library/Extensions/ > kexts-ls.txt
+
+ls /Volumes/EFI/EFI/CLOVER/drivers64UEFI/ > kexts-drivers64uefi.txt
+ls /Volumes/EFI/EFI/CLOVER/drivers64/ > kexts-drivers64.txt
