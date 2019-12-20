8328665f2a01092f332e8715588aca88ce2c34b4
Jamie Wilkinson <jamie@jamiedubs.com>
2019-12-20 11:32:09 -0500

    Trying some new kext debugging

--- list-kexts.sh
+++ list-kexts.sh
@@ -10,3 +10,5 @@ ls /Volumes/EFI/EFI/CLOVER/drivers64UEFI/ > kexts-drivers64uefi.txt
 ls /Volumes/EFI/EFI/CLOVER/drivers64/ > kexts-drivers64.txt
 
 ls /Volumes/EFI/EFI/CLOVER/kexts/Other > kexts-clover-other.txt
+
+ls /Volumes/EFI/EFI/CLOVER/Drivers/UEFI/ > kexts-drivers-uefi-new.txt
